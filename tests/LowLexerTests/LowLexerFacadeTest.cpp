ffe59a6723a6ce895b8d2a4455b3533488fdbf2e
Mikhail <glizerin@gmail.com>
2019-05-10 23:48:12 +0300

    [LowLexer] Доработал тесты класса LowLexerFacade

--- tests/LowLexerTests/LowLexerFacadeTest.cpp
+++ tests/LowLexerTests/LowLexerFacadeTest.cpp
@@ -8,13 +8,8 @@ using namespace lng::token;
 namespace
 {
 	using LexerFacadeTestCase = AbstractTestCase<std::string_view, vector<OptToken>>;
-}
-
-TEST_CASE("LowLexerFacade", "[lowlexer]")
-{
-	SECTION("can read tokens")
-	{
-		LexerFacadeTestCase testCase = {
+	
+	LexerFacadeTestCase testCase = {
 			" \n + 'literal' identifier 12345 ",
 			{
 				Token{TT_PLUS, {3, "+"}},
@@ -23,9 +18,38 @@ TEST_CASE("LowLexerFacade", "[lowlexer]")
 				Token{TT_NUMBER, {26, "12345"}},
 				nullopt,
 			}
-		};
+	};
+}
 
+TEST_CASE("LowLexerFacade", "[lowlexer]")
+{
+	SECTION("can read tokens")
+	{
 		LowLexerFacade lexer(testCase.data);
+
+		for (const auto &expected : testCase.expected)
+		{
+			REQUIRE(lexer.Next() == expected);
+		}
+	}
+
+	SECTION("can peek one token")
+	{
+		LowLexerFacade lexer(testCase.data);
+
+		REQUIRE(lexer.Peek() == testCase.expected[0]);
+		REQUIRE(lexer.Next() == testCase.expected[0]);
+	}
+
+	SECTION("can peek several tokens")
+	{
+		LowLexerFacade lexer(testCase.data);
+		auto peekedTokens = lexer.Peek(4).value();
+
+		for (size_t i = 0; i < peekedTokens.size(); ++i)
+		{
+			REQUIRE(peekedTokens[i] == testCase.expected[i]);
+		}
 
 		for (const auto &expected : testCase.expected)
 		{
